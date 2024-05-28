4260c9a279c8f0cdce0c8c28b66292ceabd2b606
Andres M <marulandaandres19@gmail.com>
2024-05-28 16:01:02 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://andresmarulandam.github.io/Responsive_Modern-UI-UX',
+  base: 'https://andresmarulandam.github.io/Responsive_Modern-UI-UX/',
 });
